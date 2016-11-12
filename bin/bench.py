b516755eec34f68498ff07823aa2d6640561286a
Gabriel D. Karpman <gdkar@alum.mit.edu>
2016-11-12 12:42:46 -0500

    c++11 requires a space before PRI{du}N

--- bin/bench.py
+++ bin/bench.py
@@ -2,29 +2,37 @@
 
 import time, sys,os, subprocess as sb
 
-target = '/var/packets.mpproto'
 if 'TARGET' in os.environ:
     target = os.environ['TARGET']
 
-count = int(sys.argv[1])
-method = sys.argv[2]
+method = sys.argv[1]
 
-turbo = 1 if method == 're2' else 6
+target = '../npu2_bench/packets.shortcut.wsproto'
+
+if method == 're2':
+    turbo = 1
+else:
+    turbo = 6
+    target += ' -r '.join('../npu2_bench/packets.shortcut_pt00{:}.wsproto'.format(x) for x in range(1,6))
+
+
+#targets = ['../npu2_bench/packets.cut.wsproto'] + [ '../npu2_bench/packets.cut_pt{:03}.wsproto'.format(x) for x in range(1,turbo)]
+#target = ' -r '.join(targets)
+#target = ' -r '.join('../npu2_bench/packets.cut_pt{x:03}.wsproto'.format(x=x) for x in range(1,turbo+1))
+#target_turbo = '../npu2_bench/packets.cut_pt00{{1..{turbo}}}.wsproto'.format(turbo=turbo)
+print('wsproto_in -r {target} | npu CONTENT -F ../npu2_bench/ -H 585 -L RESULT'.format(target=target))
 def run_npu():
     start = time.time()
-    retval = sb.call(['./waterslide','wsproto_in -r {target} | calc "#PASS=(#COUNT++ < {count})" | npu CONTENT -F ../npu2_bench/ -H 386 -L RESULT'.format(target=target,count=count * turbo)])
+    retval = sb.call(['./waterslide','wsproto_in -r {target} | npu CONTENT -F ../npu2_bench/ -H 585 -L RESULT'.format(target=target)])
     end = time.time()
     print('r:{}'.format((end-start)/turbo))
 
 def run_vectormatchre2():
     start = time.time()
-    retval = sb.call(['./waterslide','wsproto_in -r {target} | calc "#PASS=(#COUNT++ < {count})" | vectormatchre2 CONTENT -F ../npu2_bench/npu2_bench_set.npup -L RESULT -M'.format(target=target,count=count*turbo)])
+    retval = sb.call(['./waterslide','wsproto_in -r {target} | vectormatchre2 CONTENT -F ../npu2_bench_set.npup -L RESULT -M'.format(target=target)])
     end = time.time()
     print('l:{}'.format((end-start)/turbo))
 
-if method == 're2':
-    while True:
-        run_vectormatchre2()
-else:
-    while True:
-        run_npu()
+function = run_vectormatchre2 if method == 're2' else run_npu
+while True:
+    function()
