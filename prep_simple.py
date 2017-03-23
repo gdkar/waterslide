a5aa93b4e7c6a96d36e6f20b1b161b006210074f
Gabriel Karpman <gdkar@alum.mit.edu>
2017-03-23 13:24:29 -0400

    make prep_simple.py better.

--- prep_simple.py
+++ prep_simple.py
@@ -1,28 +1,47 @@
 #! /usr/bin/env python
 
 import sys, subprocess as sb, re
-import posix, posixpath as ppath
+import posix, posixpath as ppath, pathlib
 if len(sys.argv) < 2:
     print("usage: {} <infile> [ <outfile> ]".format(sys.argv[0]))
     sys.exit(-1)
 
-odir = sys.argv[2] if (len(sys.argv) > 2) else (sys.argv[1] + '.preproc')
-ofile = ppath.join(odir,sys.argv[1])
-posix.mkdir(odir)
+odir = pathlib.Path(sys.argv[2] if (len(sys.argv) > 2) else (sys.argv[1] + '.preproc'))
+ifile = pathlib.Path(sys.argv[1])
+ofile = odir.joinpath(ifile.name)
+od = ofile.parent
+parts = []
+while not od.exists():
+    parts.append(od)
+    od = od.parent
+for od in parts:
+    od.mkdir()
+#od = pathlib.Path(odir.parts[0])
+#for d in od.parts[1:]:
+#    od = od.joinpath(d)
+#    if not od.exists():
+#        od.mkdir()
+#dirs = []
+#while not od.exists():
+#    dirs.append(od.name)
+#    od = od.parent
+#for d in reversed(dirs):
+#    od.with_name(d).mkdir()
+
 count = 0
 expr = re.compile('"(.*)"(\\s*\\(.*?\\))?')
-with open(sys.argv[1]) as _ifile:
-    with open(ofile,'w') as _ofile:
+with ifile.open('rb') as _ifile:
+    with ofile.open('wb') as _ofile:
         for expression in _ifile:
             try:
                 expression = expression[:-1]
                 print(expression)
                 label = " (L{})".format(count)
-                binfile = ppath.join(odir,'expression-{}.npup'.format(count))
-                retcode = sb.call(['dc','-e',expression,'-o',binfile])
+                binfile = odir.joinpath( 'expression-{}.npup'.format(count))
+                retcode = sb.call(['dc','-e',expression,'-o',binfile.as_posix()])
                 if retcode < 0:
                     continue
-                _ofile.write('"{}"\t{} "{}"\n'.format(expression,label, binfile));
+                _ofile.write('"{}"\t{} "{}"\n'.format(expression,label, binfile.as_posix()));
                 count += 1
                 if retcode > 0:
                     print("non fatal error {}".format(retcode))
