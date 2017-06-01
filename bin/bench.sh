73c40fa929644f72390967040b5e0883f3484f5a
Duncan Townsend <git@duncancmt.com>
2017-06-01 16:06:45 -0400

    Make bench.sh take a target argument

--- bin/bench.sh
+++ bin/bench.sh
@@ -2,9 +2,12 @@
 
 cd $(dirname $(realpath $0))
 
-(./bench.py -m vectormatchre2 2>/dev/null) &
+TARGET="$1"
+shift
+
+(./bench.py -m vectormatchre2 --target="$TARGET" 2>/dev/null) &
 RE2_PID=$!
-(./bench.py -m vectormatchnpu 2>/dev/null) &
+(./bench.py -m vectormatchnpu --target="$TARGET" 2>/dev/null) &
 NPU_PID=$!
 trap 'kill -9 $NPU_PID $RE2_PID' EXIT
 wait $RE2_PID $NPU_PID
