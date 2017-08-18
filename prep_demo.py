7688c48f20bd1dc408523f0bdbd7b645d7ad0f5c
Gabriel D. Karpman <gdkar@alum.mit.edu>
2017-08-18 16:42:39 -0400

    prep_demo.py: make more better

--- prep_demo.py
+++ prep_demo.py
@@ -1,34 +1,55 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 
 import sys, subprocess as sb, re
 import posix, posixpath as ppath
-import argparse
+import argparse,pathlib
 
 parser = argparse.ArgumentParser('a ytility to precompile vectormatchnpu format config files for faster load')
-parser.add_argument('-i','--input',action='store',help='input config file')
+parser.add_argument('input',action='store',help='input config file')
 parser.add_argument('-o','--output',action='store',default=None,help='output directory')
+parser.add_argument('-a','--absolute',action='store_true',help='use absolute paths')
+parser.add_argument('--simple',action='store_true',help='use simple formating')
 parser.add_argument('args',nargs=argparse.REMAINDER)
 
 args = parser.parse_args()
 
-odir = args.output or args.input + '.preproc'
+odir = pathlib.Path(args.output or args.input + '.preproc')
 
-ofile = ppath.join(odir,ppath.basename(args.input))
+if args.absolute:
+    odir = odir.resolve().absolute()
 
-posix.mkdir(odir)
+ifile = pathlib.Path(args.input)
+ofile = odir.joinpath(ifile.name)
+od = ofile.parent
+parts = []
+while not od.exists():
+    parts.append(od)
+    od = od.parent
+for od in parts:
+    od.mkdir()
+
+#posix.mkdir(odir)
 count = 0
 expr = re.compile('"(.*)"(\\s*\\(.*?\\))?')
-with open(sys.argv[1]) as _ifile:
-    with open(ofile,'w') as _ofile:
+with ifile.open('rb') as _ifile:
+    with ofile.open('wb') as _ofile:
         for line in _ifile:
             try:
-                m = expr.match(line)
-                expression = m.group(1).replace('/','\\x2f')
-                print(expression)
-                label = m.group(2) or " (L{})".format(count)
-                binfile = ppath.join(odir,'{}-{}'.format(count,expression))
-                sb.check_call(['dc','-s','16','-e',expression,'-o',binfile] + args.args)
-                _ofile.write('"{}"\t{} "{}"\n'.format(expression,label, binfile));
+                if args.simple:
+                    expression = line[:-1]
+                    label = " (L{})".format(count)
+                else:
+                    m = expr.match(line)
+                    expression = m.group(1).replace('/','\\x2f')
+                    label = m.group(2) or " (L{})".format(count)
+
+                binfile = odir.joinpath( 'expression-{}.npup'.format(count))
+                retcode = sb.call(['dc','-q','12','-e',expression,'-o',binfile.as_posix()] + args.args)
+                if not binfile.exists():
+                    print('FAILURE: {} "{}" returned {}'.format(retcode, expression, retcode))
+                    continue
+
+                _ofile.write('"{}"\t{} "{}"\n'.format(expression,label, binfile.as_posix()));
                 count += 1
             except Exception as e:
-                print(e)
+                    print(e)
